8f4264b4515269faa9926f5d25667b1c64793d13
Utkan Güngördü <utkan@freeconsole.org>
2014-02-02 15:53:43 +0900

    fix mm3 tests, cosmetics

---
+++ mmh3/hack.go
@@ -0,0 +1,14 @@
+package mmh3
+
+import (
+	"reflect"
+	"unsafe"
+)
+
+func byteSlice(s string) (b []byte) {
+	pbytes := (*reflect.SliceHeader)(unsafe.Pointer(&b))
+	pstring := (*reflect.StringHeader)(unsafe.Pointer(&s))
+	pbytes.Data = pstring.Data
+	pbytes.Len = pstring.Len
+	return
+}
--- mmh3/mmh3_test.go
+++ mmh3/mmh3_test.go
@@ -2,22 +2,24 @@ package mmh3
 
 import (
 	"testing"
-	"fmt"
 )
 
 func TestAll(t *testing.T) {
-  s := []byte("hello")
-  if Hash32(s) != 0x248bfa47 {
-    t.Fail()
-  }
-  if fmt.Sprintf("%x", Hash128(s)) != "029bbd41b3a7d8cb191dae486a901e5b" {
-    t.Fail()
-  }
-  s = []byte("Winter is coming")
-  if Hash32(s) != 0x43617e8f {
-    t.Fail()
-  }
-  if fmt.Sprintf("%x", Hash128(s)) != "95eddc615d3b376c13fb0b0cead849c5" {
-    t.Fail()
-  }
+	s := "hello"
+	if Hash32(s) != 0x248bfa47 {
+		t.Fail()
+	}
+	h1, h2 := Hash128(s)
+	if h1 != 0xcbd8a7b341bd9b02 || h2 != 0x5b1e906a48ae1d19 {
+		t.Fail()
+	}
+	s = "Winter is coming"
+	if Hash32(s) != 0x43617e8f {
+		t.Fail()
+	}
+	h1, h2 = Hash128(s)
+
+	if h1 != 0x6c373b5d61dced95 || h2 != 0xc549d8ea0c0bfb13 {
+		t.Fail()
+	}
 }
