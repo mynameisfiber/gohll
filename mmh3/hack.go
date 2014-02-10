4352be83df4a0d09a2c220cf512cc9c75c29c526
Utkan Güngördü <utkan@freeconsole.org>
2014-02-10 14:17:59 +0900

    cosmetic changes, replace more muls with bitshifts (6g doesn't)

--- mmh3/hack.go
+++ mmh3/hack.go
@@ -5,10 +5,11 @@ import (
 	"unsafe"
 )
 
-func byteSlice(s string) (b []byte) {
+func byteSlice(s string) []byte {
+	var b []byte
 	pbytes := (*reflect.SliceHeader)(unsafe.Pointer(&b))
 	pstring := (*reflect.StringHeader)(unsafe.Pointer(&s))
 	pbytes.Data = pstring.Data
 	pbytes.Len = pstring.Len
-	return
+	return b
 }
--- mmh3/mmh3.go
+++ mmh3/mmh3.go
@@ -7,6 +7,11 @@ import (
 	"encoding/binary"
 )
 
+const (
+	c1_32 uint32 = 0xcc9e2d51
+	c2_32 uint32 = 0x1b873593
+)
+
 func Hash32(s string) uint32 {
 	length := len(s)
 	if length == 0 {
@@ -14,21 +19,20 @@ func Hash32(s string) uint32 {
 	}
 	key := byteSlice(s)
 
-	var c1, c2 uint32 = 0xcc9e2d51, 0x1b873593
-	nblocks := length / 4
+	nblocks := length >> 2
 	var h, k uint32
 	buf := bytes.NewBuffer(key)
 	for i := 0; i < nblocks; i++ {
 		binary.Read(buf, binary.LittleEndian, &k)
-		k *= c1
+		k *= c1_32
 		k = (k << 15) | (k >> (32 - 15))
-		k *= c2
+		k *= c2_32
 		h ^= k
 		h = (h << 13) | (h >> (32 - 13))
-		h = (h * 5) + 0xe6546b64
+		h = (h << 2) + h + 0xe6546b64
 	}
 	k = 0
-	tailIndex := nblocks * 4
+	tailIndex := nblocks << 2
 	switch length & 3 {
 	case 3:
 		k ^= uint32(key[tailIndex+2]) << 16
@@ -38,9 +42,9 @@ func Hash32(s string) uint32 {
 		fallthrough
 	case 1:
 		k ^= uint32(key[tailIndex])
-		k *= c1
+		k *= c1_32
 		k = (k << 15) | (k >> (32 - 15))
-		k *= c2
+		k *= c2_32
 		h ^= k
 	}
 	h ^= uint32(length)
@@ -52,6 +56,11 @@ func Hash32(s string) uint32 {
 	return h
 }
 
+const (
+	c1_128 = 0x87c37b91114253d5
+	c2_128 = 0x4cf5ad432745937f
+)
+
 func Hash128(s string) (uint64, uint64) {
 	length := len(s)
 	if length == 0 {
@@ -59,30 +68,29 @@ func Hash128(s string) (uint64, uint64) {
 	}
 	key := byteSlice(s)
 
-	var c1, c2 uint64 = 0x87c37b91114253d5, 0x4cf5ad432745937f
-	nblocks := length / 16
+	nblocks := length >> 4
 	var h1, h2, k1, k2 uint64
 	buf := bytes.NewBuffer(key)
 	for i := 0; i < nblocks; i++ {
 		binary.Read(buf, binary.LittleEndian, &k1)
 		binary.Read(buf, binary.LittleEndian, &k2)
-		k1 *= c1
+		k1 *= c1_128
 		k1 = (k1 << 31) | (k1 >> (64 - 31))
-		k1 *= c2
+		k1 *= c2_128
 		h1 ^= k1
 		h1 = (h1 << 27) | (h1 >> (64 - 27))
 		h1 += h2
-		h1 = h1*5 + 0x52dce729
-		k2 *= c2
+		h1 = (h1 << 2) + h1 + 0x52dce729
+		k2 *= c2_128
 		k2 = (k2 << 33) | (k2 >> (64 - 33))
-		k2 *= c1
+		k2 *= c1_128
 		h2 ^= k2
 		h2 = (h2 << 31) | (h2 >> (64 - 31))
 		h2 += h1
-		h2 = h2*5 + 0x38495ab5
+		h2 = (h2 << 2) + h2 + 0x38495ab5
 	}
 	k1, k2 = 0, 0
-	tailIndex := nblocks * 16
+	tailIndex := nblocks << 4
 	switch length & 15 {
 	case 15:
 		k2 ^= uint64(key[tailIndex+14]) << 48
@@ -104,9 +112,9 @@ func Hash128(s string) (uint64, uint64) {
 		fallthrough
 	case 9:
 		k2 ^= uint64(key[tailIndex+8])
-		k2 *= c2
+		k2 *= c2_128
 		k2 = (k2 << 33) | (k2 >> (64 - 33))
-		k2 *= c1
+		k2 *= c1_128
 		h2 ^= k2
 		fallthrough
 	case 8:
@@ -132,9 +140,9 @@ func Hash128(s string) (uint64, uint64) {
 		fallthrough
 	case 1:
 		k1 ^= uint64(key[tailIndex])
-		k1 *= c1
+		k1 *= c1_128
 		k1 = (k1 << 31) | (k1 >> (64 - 31))
-		k1 *= c2
+		k1 *= c2_128
 		h1 ^= k1
 	}
 	h1 ^= uint64(length)
